59eeef80ac9a335f61666d84f3c614eeef1903da
Lev-Dolzhenko <WorkSpace2707@yandex.ru>
2023-10-08 18:39:39 +0300

    wrote backend

--- js/main.js
+++ js/main.js
@@ -1,6 +1,43 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 
-btnDarkMode.onclick = function() {
-    console.log(btnDarkMode);
-    btnDarkMode.classList.toggle('dark-mode-btn--active');
+
+
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark-mode");
 }
+
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark-mode");
+} else if (localStorage.getItem('darkMode') === 'light') {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove("dark-mode");
+}
+
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+        const newColorScheme = event.matches ? "dark" : "light";
+
+        if (newColorScheme === "dark") {
+            btnDarkMode.classList.add("dark-mode-btn--active");
+            document.body.classList.add("dark-mode");
+            localStorage.setItem("darkMode", "dark");
+        } else {
+            btnDarkMode.classList.remove("dark-mode-btn--active");
+            document.body.classList.remove("dark-mode");
+            localStorage.setItem("darkMode", "light");
+        }
+    });
+
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark-mode");
+
+    if (isDark) {
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        localStorage.setItem("darkMode", "light");
+    }
+};
